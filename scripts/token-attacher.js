877f4b9d6db7e4139bc98144728d760a155fa672
KayelGee <kayelGee2@gmail.com>
2021-06-15 12:59:28 +0200

    fix typo in QuickEdit that prevents saving of offsets

--- scripts/token-attacher.js
+++ scripts/token-attacher.js
@@ -2188,7 +2188,7 @@ import {libWrapper} from './shim.js';
 			for (const key in quickEdit.elements) {
 				const layer = canvas.getLayerByEmbeddedName(key);
 				updates[key] = quickEdit.elements[key].map(elem =>{
-					let element = TokenAttacher.layerGetElement(layer, _id);
+					let element = TokenAttacher.layerGetElement(layer, elem._id);
 					//unset offset locally because I've set it locally so the user see's the effects immediatly
 					setProperty(element.data, `flags.${moduleName}.offset`, {});
 					return {_id:elem._id, [`flags.${moduleName}.offset`]: elem.offset};
