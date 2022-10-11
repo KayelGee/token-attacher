853edc6ffd64cefe4b0e400e087de817f510283d
KayelGee <kayelGee2@gmail.com>
2022-10-11 12:15:29 +0200

    Move legacy wall height code

--- scripts/compatiblity/wall-height.js
+++ scripts/compatiblity/wall-height.js
@@ -14,18 +14,30 @@
 		setProperty(options, `${moduleNameTA}.attachmentsNeedUpdate`, true);
 	}
 
-	const myLayer = "SomeLayerName";
-	function layerGetElement(layer, id, result){
-		if(layer != myLayer) return;
+	// const myLayer = "SomeLayerName";
+	// function layerGetElement(layer, id, result){
+	// 	if(layer != myLayer) return;
 
-		//Implement your get method here if your elements live in a non standard layer
-		//result.element = someobject.get(id);
-	}
+	// 	//Implement your get method here if your elements live in a non standard layer
+	// 	//result.element = someobject.get(id);
+	// }
 
 	function getElementOffset(type, objData, base_type, baseDoc, grid, offset){
 		if(!["Wall"].includes(type)) return;
 
 		let  base_elevation = baseDoc.elevation ?? baseDoc.flags['levels']?.elevation ?? baseDoc.flags['levels']?.rangeBottom ?? baseDoc.flags['wallHeight']?.wallHeightBottom ?? baseDoc.flags['wall-height']?.bottom ?? 0;
+
+		//Legacy code
+		if(objData.flags['wallHeight']?.hasOwnProperty('wallHeightTop')){				
+			offset.elevation.flags['wallHeight'] = {
+				wallHeightTop:objData.flags['wallHeight'].wallHeightTop, 
+				wallHeightBottom:objData.flags['wallHeight'].wallHeightBottom
+			};
+			
+			if([null, Infinity, -Infinity].includes(offset.elevation.flags['wallHeight'].wallHeightTop) === false) offset.elevation.flags['wallHeight'].wallHeightTop -= base_elevation;
+			if([null, Infinity, -Infinity].includes(offset.elevation.flags['wallHeight'].wallHeightBottom) === false) offset.elevation.flags['wallHeight'].wallHeightBottom -= base_elevation;
+		}
+
 		if(objData.flags[moduleName]?.hasOwnProperty('top')){				
 			offset.elevation.flags[moduleName] = {
 				top:objData.flags[moduleName].top, 
@@ -38,8 +50,11 @@
 	}
 	
 	function offsetPositionOfElement(type, objData, baseType, baseData, baseOffset, update){
+		if(!["Wall"].includes(type)) return;
+		
 		const offset = getProperty(objData, `flags.${moduleNameTA}.offset`);
 
+		//Legacy code
 		if(offset.elevation?.flags?.wallHeight?.hasOwnProperty('wallHeightBottom') || offset.elevation?.flags?.wallHeight?.hasOwnProperty('wallHeightTop')){
 			const wallHeightModule = game.modules.get(moduleName) ?? {version:0};
 			if(isNewerVersion("4.0", wallHeightModule.version)){
@@ -51,6 +66,7 @@
 				if([null, Infinity, -Infinity].includes(offset.elevation?.flags?.wallHeight?.wallHeightTop) === false) update[`flags.${moduleName}.top`] = baseOffset.elevation + offset.elevation.flags.wallHeight.wallHeightTop;
 			}
 		}
+		
 		if(offset.elevation?.flags?.[moduleName]?.hasOwnProperty('bottom') || offset.elevation?.flags?.[moduleName]?.hasOwnProperty('top')){
 			if([null, Infinity, -Infinity].includes(offset.elevation?.flags?.[moduleName]?.bottom) === false) update[`flags.${moduleName}.bottom`] = baseOffset.elevation + offset.elevation.flags[moduleName].bottom;
 			if([null, Infinity, -Infinity].includes(offset.elevation?.flags?.[moduleName]?.top) === false) update[`flags.${moduleName}.top`] = baseOffset.elevation + offset.elevation.flags[moduleName].top;
@@ -58,7 +74,7 @@
 	}
 
 	Hooks.on(`${moduleNameTA}.doAttachmentsNeedUpdate`, doAttachmentsNeedUpdate);
-	Hooks.on(`${moduleNameTA}.layerGetElement`, layerGetElement);
+	//Hooks.on(`${moduleNameTA}.layerGetElement`, layerGetElement);
 	Hooks.on(`${moduleNameTA}.getElementOffset`, getElementOffset);
 	Hooks.on(`${moduleNameTA}.offsetPositionOfElement`, offsetPositionOfElement);
 	TokenAttacher.registerHooks();
